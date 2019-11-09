4bb52e97cc6bf3f5b17e54f78d9701cf88dd6862
Kristoffer Olsson <kristoffer.olsson@cgi.com>
2019-11-09 01:16:28 +0100

    Updated tests.

--- InteractR.Resolver.Ninject.Tests/NinjectTests.cs
+++ InteractR.Resolver.Ninject.Tests/NinjectTests.cs
@@ -1,9 +1,9 @@
 using System.Threading;
 using System.Threading.Tasks;
-using InteractorHub.Interactor;
-using InteractorHub.Notification;
 using InteractorHub.Resolvers.Ninject;
-using InteractorHub.Tests.Mocks;
+using InteractR;
+using InteractR.Interactor;
+using InteractR.Resolver.AutoFac.Tests.Mocks;
 using Ninject;
 using NSubstitute;
 using NUnit.Framework;
@@ -15,49 +15,25 @@ namespace InteractorHub.Tests.Resolvers.Ninject
     {
 
         private IKernel _kernel;
-        private IHub _hub;
-        private IInteractor<MockInteractionRequest, MockResponse> _useCaseInteractor;
-        private INotificationListener<MockNotification> _notificationListener1;
-        private INotificationListener<MockNotification> _notificationListener2;
-        private INotificationListener<MockNotification> _notificationListener3;
+        private IInteractorHub _interactorHub;
+        private IInteractor<MockUseCase, IMockOutputPort> _useCaseInteractor;
         [SetUp]
         public void Setup()
         {
 
             _kernel = new StandardKernel();
-            _useCaseInteractor = Substitute.For<IInteractor<MockInteractionRequest, MockResponse>>();
-            _notificationListener1 = Substitute.For<INotificationListener<MockNotification>>();
-            _notificationListener2 = Substitute.For<INotificationListener<MockNotification>>();
-            _notificationListener3 = Substitute.For<INotificationListener<MockNotification>>();
-
-            _kernel.Bind<INotificationListener<MockNotification>>().ToMethod(context => _notificationListener1);
-            _kernel.Bind<INotificationListener<MockNotification>>().ToMethod(context => _notificationListener2);
-            _kernel.Bind<IInteractor<MockInteractionRequest, MockResponse>>()
+            _useCaseInteractor = Substitute.For<IInteractor<MockUseCase, IMockOutputPort>>();
+            _kernel.Bind<IInteractor<MockUseCase, IMockOutputPort>>()
                 .ToMethod(context => _useCaseInteractor);
 
-            _hub = new Hub(new NinjectResolver(_kernel));
+            _interactorHub = new Hub(new NinjectResolver(_kernel));
         }
 
         [Test]
         public async Task Test_Ninject_Resolver()
         {
-            await _hub.Handle<MockResponse, MockInteractionRequest>(new MockInteractionRequest());
-            await _useCaseInteractor.Received().Handle(Arg.Any<MockInteractionRequest>(), Arg.Any<CancellationToken>());
-        }
-
-        [Test]
-        public async Task Test_Ninject_NotificationListeners()
-        {
-            await _hub.Send(new MockNotification());
-            await _notificationListener1.Received().Handle(Arg.Any<MockNotification>(), Arg.Any<CancellationToken>());
-            await _notificationListener2.Received().Handle(Arg.Any<MockNotification>(), Arg.Any<CancellationToken>());
-        }
-
-        [Test]
-        public async Task Test_Ninject_NotificationListener_NotCalled()
-        {
-            await _hub.Send(new MockNotification());
-            await _notificationListener3.DidNotReceive().Handle(Arg.Any<MockNotification>(), Arg.Any<CancellationToken>());
+            await _interactorHub.Execute(new MockUseCase(), (IMockOutputPort)new MockOutputPort());
+            await _useCaseInteractor.Received().Execute(Arg.Any<MockUseCase>(),Arg.Any<IMockOutputPort>(), Arg.Any<CancellationToken>());
         }
     }
 }
